80c78947054086ab7e97464fbfffac504eb79c39
Maurício Caminha <mauricio.caminha@hotmail.com>
2024-05-18 14:21:08 -0300

    feat: ✨add: feature flag para /eventos e testes de /eventos

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -1,12 +1,18 @@
 import Evento from '../models/evento.js';
 
 class EventosController {
+  static liberaAcessoEventos = () => process.env.EVENTO_FLAG === 'true';
+
   static listarEventos = async (req, res) => {
-    try {
-      const eventos = await Evento.pegarEventos();
-      return res.status(200).json(eventos);
-    } catch (err) {
-      return res.status(500).json(err.message);
+    if (this.liberaAcessoEventos()) {
+      try {
+        const eventos = await Evento.pegarEventos();
+        return res.status(200).json(eventos);
+      } catch (err) {
+        return res.status(500).json(err.message);
+      }
+    } else {
+      return res.status(404).send();
     }
   };
 }
